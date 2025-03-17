800de143c37a9819510df3bc79ad71d6665bbf0c
CaioUSC <Caio@Carmassi.com.br>
2025-03-17 01:21:38 -0300

    Add files via upload

---
+++ Aula 1/Exercicios/Exercicio Extra/Conta_componentes.cpp
@@ -0,0 +1,34 @@
+//Conta componentes unicos
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <opencv2/opencv.hpp>
+ using namespace std;
+ using namespace cv;
+ int CountElementos(Mat_<uchar> a, int ls, int cs) {
+  int count;	 
+  queue<int> q;
+  q.push(ls); q.push(cs); //(1)
+  while (!q.empty()) { //(2)
+    int l=q.front(); q.pop(); //(3)
+    int c=q.front(); q.pop(); //(3)
+    if (a(l,c)==0) { //(4)
+      a(l,c)=255; //(5)
+      q.push(l-1); q.push(c); //6-acima
+      q.push(l+1); q.push(c); //6-abaixo
+      q.push(l); q.push(c+1); //6-direita
+      q.push(l); q.push(c-1); //6-esq
+    }
+  }
+ }
+ int main() {
+  int count = 0;	 
+  Mat_<uchar> a=imread("c32.bmp",1);
+  for (int l=1; l<a.rows-1; l++)
+	for (int c=1; c<a.cols-1; c++)
+		if(a(l,c)==0){
+		CountElementos(a,l,c);
+		count++;
+		}
+  cout << "Número de componentes conexos: " << count << endl;
+ }
