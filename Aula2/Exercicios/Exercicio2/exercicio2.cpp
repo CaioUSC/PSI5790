81ffb629a91ce4cc10bd91c9f668d1fc42cb38c4
CaioUSC <Caio@Carmassi.com.br>
2025-03-24 12:42:02 -0300

    Exercicio 2
    Finalização do exercicio 2 e arrumando pasta

--- Aula2/Exercicios/Exercicio2/exercicio2.cpp
+++ Aula2/Exercicios/Exercicio2/exercicio2.cpp
@@ -1,13 +1,16 @@
  // exercicio2.cpp 
  #include "procimagem.h"
  
- Mat_<Vec3f> marca(Mat_<float> a, Mat_<float> p, float limiar) {
+ Mat_<Vec3f> marca(Mat_<float> a, Mat_<float> p,Mat_<float> pi, float limiar) {
 	Mat_<Vec3f> d;
 	cvtColor(a,d,COLOR_GRAY2BGR);
 	for (int l=0; l<a.rows; l++)
-		for (int c=0; c<a.cols; c++)
+		for (int c=0; c<a.cols; c++){
 		if (p(l,c)>=limiar)
 			rectangle(d,Point(c,l),Point(c,l),Scalar(0.0,0.0,1.0),3);
+		if (pi(l,c)>=limiar)
+			rectangle(d,Point(c,l),Point(c,l),Scalar(0.0,0.0,1.0),3);	
+		}
  return d;
  }
  
@@ -20,13 +23,10 @@
   // Executa template match CCN
   Mat_<float> p=matchTemplateSame(a, q, TM_CCOEFF_NORMED);
   //Encontra os pontos onde o fundo e o urso são iguais o template
-  Mat_<float> pontos= p;
-  resultado = marca(a,pontos,0.75);
-  imwrite("resultado1.png",255*resultado);
+  Mat_<float> pontos = p;
   //Encontra os pontos onde o fundo e o urso tem as cores invertidas do template
-  Mat_<float> resultado1 = imread("resultado1.png",0);
-  resultado1 = resultado1/255;
   Mat_<float> pontos_invertidos = -1*p;
-  resultado = marca(resultado1,pontos_invertidos,0.75);
-  imwrite("resultado2.png",255*resultado);
+  //Marca os pontos que cruzam o liminar definido
+  resultado = marca(a,pontos,pontos_invertidos,0.75);
+  imwrite("resultado.png",255*resultado);
  }
