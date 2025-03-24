21a32900d4af0fb409ced9e84e242176c3f0c8c9
CaioUSC <Caio@Carmassi.com.br>
2025-03-24 00:15:52 -0300

    Add files via upload

---
+++ Aula2/Exercicios/Exercicio1/Mediana_ocv.cpp
@@ -0,0 +1,16 @@
+//mediana_ocv.cpp - 2024
+ #include <opencv2/opencv.hpp>
+ using namespace std;
+ using namespace cv;
+ 
+ int main(){
+	 Mat_<uchar> a = imread("fever-1.pgm",0);
+	 Mat_<uchar> b;
+	 medianBlur(a,b,5);
+	 imwrite("fever1Mediana.png",b);
+	 
+	 Mat_<uchar> c = imread("fever-2.pgm",0);
+	 Mat_<uchar> d;
+	 medianBlur(c,d,11);
+	 imwrite("fever2Mediana.png",d);
+ }
